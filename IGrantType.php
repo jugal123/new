3b8a90428fb3b0267741b8c30a69222efa4f8475
jugal123 <jugal@alchemytechnologies.ca>
2014-07-19 14:13:58 +0530

    Update IGrantType.php

--- IGrantType.php
+++ IGrantType.php
@@ -3,4 +3,5 @@ $redirectUrl    = 'http://www.myapp.com/oauth_callback';
 $authorizeUrl   = 'https://foobar.nationbuilder.com/oauth/authorize';
 $authUrl = $client->getAuthenticationUrl($authorizeUrl, $redirectUrl);
 echo $authUrl;
+echo 'hellodear';
 ?>
